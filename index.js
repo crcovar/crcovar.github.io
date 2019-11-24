d297cd5150eaaab5ad71187136fa58e3919036a4
Charles Covar <covar1@gmail.com>
2019-11-24 13:25:32 -0500

    First pass at product_select event

--- index.js
+++ index.js
@@ -10,50 +10,69 @@ function docReady(fn) {
 	}
 }
 
+function tagProductListItems() {
+	// product list name
+	let list_name = document.querySelector('.product-list h2:first-of-type').innerText.toLowerCase();
+
+	function selectContentClickHandler(item) {
+		gtag('event', 'select_content', {
+			content_type: 'product',
+			items: [item],
+		});
+	}
+
+	function buildItemList(listItems) {
+		return listItems.map((li, index) => {
+			let nameAndPrice = li.innerText.split('USD $');
+			let name = nameAndPrice[0].trim();
+			let price = parseFloat(nameAndPrice[1]);
+
+			let item = { name, price, list_name, list_position: index + 1 };
+
+			li.querySelectorAll('a.all_product').forEach(a => {
+				a.onclick = selectContentClickHandler(item);
+			});
+
+			return item;
+		});
+	}
+	// get product list
+	let items = buildItemList([...document.querySelectorAll('.product-list #ulAllProduct li')]);
+	if (items.length === 0) {
+		// not on a product list page so check for main page list
+		items = buildItemList([...document.querySelectorAll('.product-list ul:not(#ulSearch) li')]);
+	}
+
+	if (items.length > 0) {
+		gtag('event', 'view_item_list', { items });
+	}
+}
+
+function tagProductDetail() {
+	let item = {
+		name: document.querySelector('.product_view .pro_detail h2').innerText.trim(),
+		price: parseFloat(document.querySelector('.product_view .pro_detail .price').innerText.replace('USD $', '')),
+	};
+
+	[...document.querySelectorAll('.product_view .pro_content li')].forEach(li => {
+		if (li.children.length > 1 && li.children[0].innerText.toLowerCase() === 'publisher') {
+			item.brand = li.children[1].innerText.trim();
+		}
+		if (li.children.length > 1 && li.children[0].innerText.toLowerCase() === 'category') {
+			item.category = li.children[1].innerText.trim();
+		}
+	});
+	gtag('event', 'view_item', { items: [item] });
+}
+
 function main() {
 	if (document.querySelector('.product-list')) {
-		// product list name
-		let list_name = document.querySelector('.product-list h2:first-of-type').innerText.toLowerCase();
-
-		function buildItemList(listItems) {
-			return listItems.map((li, index) => {
-				let item = li.innerText.split('USD $');
-				let name = item[0].trim();
-				let price = parseFloat(item[1]);
-
-				return { name, price, list_name, list_position: index + 1 };
-			});
-		}
-		// get product list
-		let items = buildItemList([...document.querySelectorAll('.product-list #ulAllProduct li')]);
-		if (items.length === 0) {
-			// not on a product list page so check for main page list
-			items = buildItemList([...document.querySelectorAll('.product-list ul:not(#ulSearch) li')]);
-		}
-
-		if (items.length > 0) {
-			gtag('event', 'view_item_list', { items });
-		}
+		tagProductListItems();
 	}
 
 	// product detail view
 	if (document.querySelector('.product_view')) {
-		let item = {
-			name: document.querySelector('.product_view .pro_detail h2').innerText.trim(),
-			price: parseFloat(
-				document.querySelector('.product_view .pro_detail .price').innerText.replace('USD $', ''),
-			),
-		};
-
-		[...document.querySelectorAll('.product_view .pro_content li')].forEach(li => {
-			if (li.children.length > 1 && li.children[0].innerText.toLowerCase() === 'publisher') {
-				item.brand = li.children[1].innerText.trim();
-			}
-			if (li.children.length > 1 && li.children[0].innerText.toLowerCase() === 'category') {
-				item.category = li.children[1].innerText.trim();
-			}
-		});
-		gtag('event', 'view_item', { items: [item] });
+		tagProductDetail();
 	}
 }
 
