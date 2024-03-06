2e4600e9a0f8e048303cdb674f54ad404fc3f75d
Charles Covar <covar1@gmail.com>
2024-03-06 10:07:28 -0500

    Ready for first deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false }), react(), mdx()],
+  site: "https://charlescovar.com",
 });
